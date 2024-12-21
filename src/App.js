6df7316d0c22be15b183062143eddb75cfa1c405
userROHITH001 <mrohith990@gmail.com>
2024-12-21 10:06:16 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -8,19 +8,19 @@ import { useState } from "react";
 const initialFriends = [
   {
     id: 118836,
-    name: "Clark",
+    name: "vijay M",
     image: "https://i.pravatar.cc/48?u=118836",
     balance: -7,
   },
   {
     id: 933372,
-    name: "Sarah",
+    name: "rohith M",
     image: "https://i.pravatar.cc/48?u=933372",
     balance: 20,
   },
   {
     id: 499476,
-    name: "Anthony",
+    name: "Saravanan N",
     image: "https://i.pravatar.cc/48?u=499476",
     balance: 0,
   },
